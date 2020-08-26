bed67175ac901fcfb06dc6ccfa9b3490933cb4ca
Charles Sieg <charles.sieg@renkara.com>
2020-08-26 15:22:28 -0500

    Reverted changes.

--- main.tf
+++ main.tf
@@ -1,69 +1,3 @@
-###########################################################
-###########################################################
-# CLIENT FILES CLOUDFRONT LAMBDA
-###########################################################
-###########################################################
-#
-# Create the IAM role and associated policies for executing the lambda function.
-#
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    effect  = "Allow"
-    principals {
-      identifiers = [
-        "edgelambda.amazonaws.com",
-        "lambda.amazonaws.com",
-      ]
-      type = "Service"
-    }
-  }
-}
-data "aws_iam_policy_document" "permissions" {
-  statement {
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-      "secretsmanager:DescribeSecret",
-      "secretsmanager:GetRandomPassword",
-      "secretsmanager:GetResourcePolicy",
-      "secretsmanager:GetSecretValue",
-      "secretsmanager:ListSecretVersionIds",
-    ]
-    effect    = "Allow"
-    resources = ["*"]
-    sid       = "DefaultDocumentMissLambdaPermissions"
-  }
-}
-resource "aws_iam_role" "lambda_execution_role" {
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-  name               = "${var.environment}-${var.app_name}-lambda-execution-role"
-}
-resource "aws_iam_role_policy" "lambda_execution_role" {
-  name   = "${var.environment}-${var.app_name}-lambda-execution-role-policy"
-  policy = data.aws_iam_policy_document.permissions.json
-  role   = aws_iam_role.lambda_execution_role.id
-}
-#
-# Zip up the lambda source code and create the lambda function.
-#
-data "archive_file" "lambda" {
-  output_path = "${path.module}/.files/defaultDocumentMiss.zip"
-  source_file = "${path.module}/source/defaultDocumentMiss/index.js"
-  type        = "zip"
-}
-resource "aws_lambda_function" "lambda" {
-  filename         = data.archive_file.lambda.output_path
-  function_name    = "${var.environment}-${var.app_name}-defaultDocumentMiss"
-  handler          = "index.handler"
-  publish          = true
-  role             = aws_iam_role.lambda_execution_role.arn
-  runtime          = "nodejs10.x"
-  source_code_hash = data.archive_file.lambda.output_base64sha256
-}
-
-
 #
 # Look up the SSL certificate in AWS Certificate Manager for the provided domain.
 #
@@ -77,21 +11,14 @@ data "aws_acm_certificate" "cert" {
 # Creates a CloudFront distribution for a given S3 bucket.
 #
 resource "aws_cloudfront_distribution" "cloudfront" {
-  origin {
-    domain_name = "${var.bucket_name}.s3.amazonaws.com"
-    origin_id   = "${var.app_name}-${var.environment}"
-
-    s3_origin_config {
-      origin_access_identity = var.origin_access_identity
-    }
-  }
-
   aliases             = [var.bucket_name] # Alternate domain names, CNAMEs
   default_root_object = "index.html"
   enabled             = true    # Distribution State
   http_version        = "http2" # Supported HTTP Versions = HTTP/2, HTTP/1.1, HTTP/1.0
   is_ipv6_enabled     = true
-  retain_on_delete    = false // if true, distribution is only disable on terraform destroy
+  price_class         = "PriceClass_100" # Only USA and Europe; least expensive option
+  retain_on_delete    = false            // if true, distribution is only disable on terraform destroy
+  wait_for_deployment = false
 
   # Logging disabled.
 
@@ -112,29 +39,31 @@ resource "aws_cloudfront_distribution" "cloudfront" {
         forward = "all"
       }
     }
+  }
 
-    lambda_function_association {
-      event_type   = "viewer-request"
-      lambda_arn   = aws_lambda_function.lambda.qualified_arn
-      include_body = false
+  origin {
+    domain_name = "${var.bucket_name}.s3.amazonaws.com"
+    origin_id   = "${var.app_name}-${var.environment}"
+
+    s3_origin_config {
+      origin_access_identity = var.origin_access_identity
     }
   }
-  # Only USA and Europe; least expensive option
-  price_class = "PriceClass_100"
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
-
-      #locations        = ["US", "CA", "GB", "DE"]
     }
   }
+
   tags = {
-    Name        = "${var.environment}-${var.app_name}-cloudfront"
     Application = "${var.environment}-${var.app_name}"
     Billing     = "${var.environment}-${var.app_name}"
-    Environment = var.environment
+    Environment = "${var.environment}"
+    Name        = "${var.environment}-${var.app_name}-cloudfront"
     Terraform   = "true"
   }
+
   viewer_certificate {
     acm_certificate_arn      = data.aws_acm_certificate.cert.arn
     minimum_protocol_version = "TLSv1.1_2016" # Security Policy; TLSv1.1_2016 (recommended)
