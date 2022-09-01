b3f4edd59700b9918398b0b7c5afe692b8af3149
Charles Sieg <crsieg@gmail.com>
2022-08-31 19:16:22 -0500

    Fixes.

--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "app_name" {
-  description = "application name"
-  type        = string
-}
-
 variable "aws_region" {
   description = "The AWS region in which the infrastructure will be provisioned."
   type        = string
@@ -22,6 +17,11 @@ variable "cloudwatch_logs_retention_in_days" {
   default     = 3
   description = "Specifies the number of days you want to retain log events in the specified log group. Possible values are: 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, 3653, and 0. If you select 0, the events in the log group are always retained and never expire."
   type        = number
+}
+
+variable "name" {
+  description = "application name"
+  type        = string
 }
 
 variable "name_prefix" {
